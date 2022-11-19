446d99f8c5c9e8613b02be152cc5649b211120ec
Vladimir <neatek@icloud.com>
2022-11-19 11:29:08 +0400

    lesson_3 Urls in templates

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,3 +1,14 @@
+# from django.urls import path
+# from mainapp import views
+# from mainapp.apps import MainappConfig
+
+# app_name = MainappConfig.name
+
+# urlpatterns = [
+#     path("", views.HelloWorldView.as_view()),
+#     path("<str:word>/", views.check_kwargs),
+# ]
+
 from django.urls import path
 from mainapp import views
 from mainapp.apps import MainappConfig
@@ -5,6 +16,10 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path("", views.HelloWorldView.as_view()),
-    path("<str:word>/", views.check_kwargs),
+    path("", views.MainPageView.as_view(), name="main_page"),
+    path("news/", views.NewsPageView.as_view(), name="news"),
+    path("courses/", views.CoursesPageView.as_view(), name="courses"),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+    path("login/", views.LoginPageView.as_view(), name="login"),
 ]

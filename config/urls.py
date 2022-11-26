6cf8aa3418995f5256e5b00630a6b5a442e9a016
Vladimir <neatek@icloud.com>
2022-11-26 14:32:34 +0400

    lesson_5 New app and user model

--- config/urls.py
+++ config/urls.py
@@ -37,6 +37,18 @@ Including another URLconf
 # ]
 
 
+# from django.contrib import admin
+# from django.urls import include, path
+# from django.views.generic import RedirectView
+
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path("", RedirectView.as_view(url="mainapp/")),
+#     path("mainapp/", include("mainapp.urls", namespace="mainapp")),
+# ]
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
@@ -46,3 +58,7 @@ urlpatterns = [
     path("", RedirectView.as_view(url="mainapp/")),
     path("mainapp/", include("mainapp.urls", namespace="mainapp")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

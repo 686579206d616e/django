fc8316002300f6dce9f2288b063ada021f980240
Vladimir <neatek@icloud.com>
2022-11-26 15:09:25 +0400

    lesson_5 Authapp done

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -34,6 +34,20 @@ app_name = MainappConfig.name
 #     path("login/", views.LoginPageView.as_view(), name="login"),
 # ]
 
+# urlpatterns = [
+#     path("", views.MainPageView.as_view(), name="main_page"),
+#     path("news/", views.NewsPageView.as_view(), name="news"),
+#     path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
+#     path("courses/", views.CoursesListView.as_view(), name="courses"),
+#     path(
+#         "courses/<int:pk>/",
+#         views.CoursesDetailView.as_view(),
+#         name="courses_detail",
+#     ),
+#     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+#     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+#     path("login/", views.LoginPageView.as_view(), name="login"),
+# ]
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
     path("news/", views.NewsPageView.as_view(), name="news"),
@@ -46,5 +60,4 @@ urlpatterns = [
     ),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
-    path("login/", views.LoginPageView.as_view(), name="login"),
 ]

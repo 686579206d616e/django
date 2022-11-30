69bead1fe17d4b0c619ef81571961667429bb98e
Vladimir <neatek@icloud.com>
2022-11-30 21:57:13 +0400

    lesson_6 News CRUD and permissions

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,57 +1,29 @@
-# from django.urls import path
-# from mainapp import views
-# from mainapp.apps import MainappConfig
-
-# app_name = MainappConfig.name
-
-# urlpatterns = [
-#     path("", views.HelloWorldView.as_view()),
-#     path("<str:word>/", views.check_kwargs),
-# ]
-
 from django.urls import path
+
 from mainapp import views
 from mainapp.apps import MainappConfig
 
 app_name = MainappConfig.name
 
-# urlpatterns = [
-#     path("", views.MainPageView.as_view(), name="main_page"),
-#     path("news/", views.NewsPageView.as_view(), name="news"),
-#     path("courses/", views.CoursesPageView.as_view(), name="courses"),
-#     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
-#     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
-#     path("login/", views.LoginPageView.as_view(), name="login"),
-# ]
-
-# urlpatterns = [
-#     path("", views.MainPageView.as_view(), name="main_page"),
-#     path("news/", views.NewsPageView.as_view(), name="news"),
-#     path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
-#     path("courses/", views.CoursesPageView.as_view(), name="courses"),
-#     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
-#     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
-#     path("login/", views.LoginPageView.as_view(), name="login"),
-# ]
-
-# urlpatterns = [
-#     path("", views.MainPageView.as_view(), name="main_page"),
-#     path("news/", views.NewsPageView.as_view(), name="news"),
-#     path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
-#     path("courses/", views.CoursesListView.as_view(), name="courses"),
-#     path(
-#         "courses/<int:pk>/",
-#         views.CoursesDetailView.as_view(),
-#         name="courses_detail",
-#     ),
-#     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
-#     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
-#     path("login/", views.LoginPageView.as_view(), name="login"),
-# ]
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
-    path("news/", views.NewsPageView.as_view(), name="news"),
-    path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
+    path("news/", views.NewsListView.as_view(), name="news"),
+    path("news/create/", views.NewsCreateView.as_view(), name="news_create"),
+    path(
+        "news/<int:pk>/detail",
+        views.NewsDetailView.as_view(),
+        name="news_detail",
+    ),
+    path(
+        "news/<int:pk>/update",
+        views.NewsUpdateView.as_view(),
+        name="news_update",
+    ),
+    path(
+        "news/<int:pk>/delete",
+        views.NewsDeleteView.as_view(),
+        name="news_delete",
+    ),
     path("courses/", views.CoursesListView.as_view(), name="courses"),
     path(
         "courses/<int:pk>/",

16cb75766e9e6bec7ece1e7a4162b5d2d0368c71
Vladimir <neatek@icloud.com>
2022-12-06 17:41:33 +0400

    lesson_7 Cache

--- config/urls.py
+++ config/urls.py
@@ -47,6 +47,7 @@ Including another URLconf
 #     path("mainapp/", include("mainapp.urls", namespace="mainapp")),
 # ]
 
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -62,4 +63,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

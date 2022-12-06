b7548b5575923fd0eb02e1b6fe5a43efd2610a96
Vladimir <neatek@icloud.com>
2022-12-06 18:46:52 +0400

    lesson_8 i18n

--- config/urls.py
+++ config/urls.py
@@ -1,52 +1,3 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-# ]
-
-
-# from django.urls import path
-# from mainapp import views
-# from mainapp.apps import MainappConfig
-
-# app_name = MainappConfig.name
-
-# urlpatterns = [
-#     path("", views.MainPageView.as_view()),
-#     path("news/", views.NewsPageView.as_view()),
-#     path("courses/", views.CoursesPageView.as_view()),
-#     path("contacts/", views.ContactsPageView.as_view()),
-#     path("doc_site/", views.DocSitePageView.as_view()),
-#     path("login/", views.LoginPageView.as_view()),
-# ]
-
-
-# from django.contrib import admin
-# from django.urls import include, path
-# from django.views.generic import RedirectView
-
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-#     path("", RedirectView.as_view(url="mainapp/")),
-#     path("mainapp/", include("mainapp.urls", namespace="mainapp")),
-# ]
-
 import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
@@ -56,6 +7,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
     path("", RedirectView.as_view(url="mainapp/")),
     path("social_auth/", include("social_django.urls", namespace="social")),
     path("mainapp/", include("mainapp.urls", namespace="mainapp")),
